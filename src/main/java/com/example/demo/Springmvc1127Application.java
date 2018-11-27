a2bdfb172536c6f93c4783e4f32c32932b8be83e
1603015539 <45160260+1603015539@users.noreply.github.com>
2018-11-27 08:53:48 +0800

    项目初始结构提交

---
+++ src/main/java/com/example/demo/Springmvc1127Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc1127Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvc1127Application.class, args);
+	}
+}
